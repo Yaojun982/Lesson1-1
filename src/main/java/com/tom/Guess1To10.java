ae361d43510f97d22b096c7d457c35f0ac865692
USER <paul881125@yahoo.com.tw>
2021-11-02 14:35:41 +0800

    gijoe

--- src/main/java/com/tom/Guess1To10.java
+++ src/main/java/com/tom/Guess1To10.java
@@ -8,19 +8,24 @@ public class Guess1To10 {
         Random random = new Random();
         int secret = random.nextInt(10) + 1;
         System.out.println(secret);
-        System.out.println("Please enter number");
+        System.out.println("Please enter number:");
         Scanner scanner = new Scanner(System.in);
-        String s = scanner.next();
-        System.out.println("user:" + s);
-        int num = Integer.parseInt(s);
-        if (num > secret) {
-            System.out.println("Smaller");
-        }else {
-            if (num < secret) {
-                System.out.println("Bigger");
-        }else{
-                System.out.println("Ok");
+        boolean playgame = true;
+        while (playgame) {
+            String s = scanner.next();
+            System.out.println("user:" + s);
+            int num = Integer.parseInt(s);
+            if (num > secret) {
+                System.out.println("Smaller");
+            } else {
+                if (num < secret) {
+                    System.out.println("Bigger");
+                } else {
+                    System.out.println("Ok");
+                    playgame = false;
+                }
             }
         }
+        System.out.println("87940");
     }
 }
