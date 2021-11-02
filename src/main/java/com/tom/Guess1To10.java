d2207b895d569783d1afd524079fe3b7f8f58c56
USER <paul881125@yahoo.com.tw>
2021-11-02 14:00:46 +0800

    Test1

---
+++ src/main/java/com/tom/Guess1To10.java
@@ -0,0 +1,26 @@
+package com.tom;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Guess1To10 {
+    public static void main(String[] args) {
+        Random random = new Random();
+        int secret = random.nextInt(10) + 1;
+        System.out.println(secret);
+        System.out.println("Please enter number");
+        Scanner scanner = new Scanner(System.in);
+        String s = scanner.next();
+        System.out.println("user:" + s);
+        int num = Integer.parseInt(s);
+        if (num > secret) {
+            System.out.println("Smaller");
+        }else {
+            if (num < secret) {
+                System.out.println("Bigger");
+        }else{
+                System.out.println("Ok");
+            }
+        }
+    }
+}
