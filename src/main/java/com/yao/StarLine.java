c4e0569d9cda2014777a70e318e223719cebe23a
Yaojun982 <paul881125@yahoo.com>
2021-11-09 13:41:34 +0800

    second mode

---
+++ src/main/java/com/yao/StarLine.java
@@ -0,0 +1,16 @@
+package com.yao;
+
+public class StarLine {
+    public static void main(String[] args) {
+        for (int i=0; i<10; i++) {
+            for (int j = 0; j < 20; j++) {
+                if (i==j || i==19-j) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
